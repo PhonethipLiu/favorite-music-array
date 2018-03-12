68ef3e169761443a981177a111380a8c69652762
Phonethip <phonethip.liu@gmail.com>
2018-03-12 02:25:58 -0500

    initial setup

---
+++ arraymethods.js
@@ -0,0 +1,25 @@
+// Use JavaScript arrays and innerHTML to show the music you love.
+
+// * Create and modify arrays with join, push, reverse, sort, concat and unshift.
+// * Output lists to the DOM.
+// *Create three separate arrays; one for **songs**, one for **albums**, and one for **artists**.
+
+const songs = [], // *The song array should be initialized with 10 songs.
+     albums = [], // *Each subsequent array should hold the corresponding data (artist and album).
+    artists = [];
+
+// * Using a form; Add 2 more songs.
+//     + Each song should be added to the end of the song array.
+//     + Each artist should be added to the beginning of the artist array.
+//     + Each album should be added to the end of the album array.
+
+// * For each output element arrange the corresponding data as follows:
+//     *   Output vertically descending alphabetically.
+//     *   Output vertically ascending alphabetically.
+//     *   Output vertically exactly as the data was added.
+//     *   Output vertically exactly as the data was added, inverted.
+//     *   Output horizontally descending alphabetically, separated by `, `.
+//     *   Output horizontally ascending alphabetically, separated by ` >> `.
+//     *   Output horizontally exactly as the data was added, separated by ` : `.
+//     *   Output horizontally exactly as the data was added, inverted, separated by ` | `.
+// * Each output area should be separated by a headline tag.
