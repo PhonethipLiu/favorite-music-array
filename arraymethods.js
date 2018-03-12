55b21f4184a09228864dbd008ebbbdffe3dc6795
Phonethip <phonethip.liu@gmail.com>
2018-03-12 16:10:15 -0500

    added arrays

--- arraymethods.js
+++ arraymethods.js
@@ -4,9 +4,11 @@
 // * Output lists to the DOM.
 // *Create three separate arrays; one for **songs**, one for **albums**, and one for **artists**.
 
-const songs = [], // *The song array should be initialized with 10 songs.
-     albums = [], // *Each subsequent array should hold the corresponding data (artist and album).
-    artists = [];
+const songs = [ "Come Away With Me", "Uptown Funky", "At Last", "Ani't No Sunshine", "Love", "Sexy Back", "Rolling In The Deep", "Back To Black ", "Fell In Love With A Boy", "Work It"], // *The song array should be initialized with 10 songs.
+
+artists = [ "Norah Jones", "Mark Ronson with Bruno Mars", "Eta James", "Bill Withers", "Matt White", "Justin Timberlake", "Adelle", "Amy Winehouse", "Joss Stone", "Miss Elliott"  ], // *Each subsequent array should hold the corresponding data (artist and album).
+
+albums = [  "At Last!, 1960","Just as I am, 1971" ,"Best Days, 2007","Futuresex/Lovesounds, 2006", "Adele 21, 2011", "Back to Black, 2006","The Soul Sessions, 2003", "Under Construction, 2002"];
 
 // * Using a form; Add 2 more songs.
 //     + Each song should be added to the end of the song array.
